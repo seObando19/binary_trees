b2e3b23da22fb6018c8abb06ccc9d818307b5d32
seObando19 <sebastiano-10@hotmail.com>
2020-11-25 07:30:50 +0000

    Completed task 5 and added test 5 and header file

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_root - function that checks if a node is a leaf
+ * @node:pointer to the node to check
+ * Return: 1 if node leaf or 0
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	if (node->left != NULL && node->right != NULL && node->parent == NULL)
+		return (1);
+	return (0);
+}
