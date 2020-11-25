7af89b271330bc6315b2f8fe8bca5b4483a404e2
seObando19 <sebastiano-10@hotmail.com>
2020-11-25 07:33:50 +0000

    Updated task 5

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -8,7 +8,7 @@ int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
-	if (node->left != NULL && node->right != NULL && node->parent == NULL)
+	if (node != NULL && node->parent == NULL)
 		return (1);
 	return (0);
 }
